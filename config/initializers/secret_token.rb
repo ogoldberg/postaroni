cde6e7a5950b4c9085611718eaf72e890a9b47b6
Oren Goldberg <ogoldberg@gmail.com>
2013-05-07 17:23:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postaroni::Application.config.secret_token = 'bc7a6728541e10fe2be7b3a49c02bad3230367c960e00543cc4b55058e8a7a4163015774b0ac0e0d1ccc22755e5728429f6fa10ebcf1b23eff95a179669f7362'
