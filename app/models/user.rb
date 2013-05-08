408e6638b3f393b51f69d33cb3d3e8f28081d597
Oren Goldberg <ogoldberg@gmail.com>
2013-05-08 18:06:41 -0400

    when a user is deleted, so are their posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, :dependent => :destroy
   has_many :comments, :through => :posts
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
