29c6a31e735a2422f8d5d95e8b7c80cc74954437
Oren Goldberg <ogoldberg@gmail.com>
2013-05-08 22:30:09 -0400

    added presence validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   	friendly_id :title, use: [:slugged, :history]
 	has_many :comments, :dependent => :destroy
 	belongs_to :user
+	validates_presence_of :content, :user_id, :title
 
 	attr_accessible :content, :title, :user_id
 end
